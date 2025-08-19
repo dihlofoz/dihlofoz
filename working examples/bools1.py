69a58af8c47f92d7b43931abd275f376983adef2
Artyom <egorvanhelsing@gmail.com>
2025-08-19 13:54:32 +0300

    Working Examples

---
+++ working examples/bools1.py
@@ -0,0 +1,8 @@
+def equilateral(sides):
+    a = input('Первая сторона:')
+    b = input('Вторая сторона:')
+    c = input('Третья сторона:')
+    if a == b == c:
+        return True
+    else:
+        return False
